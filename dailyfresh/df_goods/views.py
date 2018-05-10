880a1ae8b49787a6ccbbfaad9a4407a5cdc71ee6
huanagxu <kevin490990685@163.com>
2018-05-11 00:09:01 +0800

    列表页部分

--- dailyfresh/df_goods/views.py
+++ dailyfresh/df_goods/views.py
@@ -27,3 +27,28 @@ def index(request):
         'list': list
     }
     return render(request, 'df_goods/index.html', context)
+
+
+def list(request, type_id, page, sort_type):
+    """
+    goodlist函数负责展示某类商品的信息。
+    :param request: 请求
+    :param type_id: 商品类型id
+    :param page: 页码
+    :param sort_type: 查询条件id 1为根据id查询 2根据价格查询 3根据点击量查询
+    :return: 响应
+    """
+
+    goods_list = GoodsInfo.objects.filter(gtype_id=type_id)
+    if sort_type == '1':
+        goods_list = goods_list.order_by('-id')
+    elif sort_type == '2':
+        goods_list = goods_list.order_by('-gprice')
+    elif sort_type == '3':
+        goods_list == goods_list.order_by('-gclick')
+
+    contex = {
+        'title': '商品详情',
+        'goods_list': goods_list,
+    }
+    return render(request, 'df_goods/list.html', contex)
